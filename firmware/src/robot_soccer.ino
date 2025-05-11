c1248888382db62932c42ee62c8386006ff98a90
Ras Rizkov <132064583+Customize5773@users.noreply.github.com>
2025-05-11 19:35:31 +0700

    Create robot_soccer.ino

---
+++ firmware/src/robot_soccer.ino
@@ -0,0 +1,75 @@
+#include "../config.h"
+
+// Bluetooth Serial
+#ifdef USE_SOFTWARE_SERIAL
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(BLUETOOTH_RX, BLUETOOTH_TX);
+#else
+#define BTSerial Serial
+#endif
+
+void setup() {
+  pinMode(MOTOR_LEFT_FWD, OUTPUT);
+  pinMode(MOTOR_LEFT_BWD, OUTPUT);
+  pinMode(MOTOR_RIGHT_FWD, OUTPUT);
+  pinMode(MOTOR_RIGHT_BWD, OUTPUT);
+  pinMode(KICKER_PIN, OUTPUT);
+
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  if (BTSerial.available()) {
+    char command = BTSerial.read();
+
+    switch (command) {
+      case 'F': moveForward(); break;
+      case 'B': moveBackward(); break;
+      case 'L': turnLeft(); break;
+      case 'R': turnRight(); break;
+      case 'S': stopMotors(); break;
+      case 'K': kickBall(); break;
+    }
+  }
+}
+
+void moveForward() {
+  digitalWrite(MOTOR_LEFT_FWD, HIGH);
+  digitalWrite(MOTOR_LEFT_BWD, LOW);
+  digitalWrite(MOTOR_RIGHT_FWD, HIGH);
+  digitalWrite(MOTOR_RIGHT_BWD, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(MOTOR_LEFT_FWD, LOW);
+  digitalWrite(MOTOR_LEFT_BWD, HIGH);
+  digitalWrite(MOTOR_RIGHT_FWD, LOW);
+  digitalWrite(MOTOR_RIGHT_BWD, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(MOTOR_LEFT_FWD, LOW);
+  digitalWrite(MOTOR_LEFT_BWD, HIGH);
+  digitalWrite(MOTOR_RIGHT_FWD, HIGH);
+  digitalWrite(MOTOR_RIGHT_BWD, LOW);
+}
+
+void turnRight() {
+  digitalWrite(MOTOR_LEFT_FWD, HIGH);
+  digitalWrite(MOTOR_LEFT_BWD, LOW);
+  digitalWrite(MOTOR_RIGHT_FWD, LOW);
+  digitalWrite(MOTOR_RIGHT_BWD, HIGH);
+}
+
+void stopMotors() {
+  digitalWrite(MOTOR_LEFT_FWD, LOW);
+  digitalWrite(MOTOR_LEFT_BWD, LOW);
+  digitalWrite(MOTOR_RIGHT_FWD, LOW);
+  digitalWrite(MOTOR_RIGHT_BWD, LOW);
+}
+
+void kickBall() {
+  digitalWrite(KICKER_PIN, HIGH);
+  delay(200);
+  digitalWrite(KICKER_PIN, LOW);
+}
